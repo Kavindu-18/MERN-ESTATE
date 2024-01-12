1641d134186ef4238b5a7191c2c86cff21b59ff3
Kavindu-18 <kavinduperera19@gmail.com>
2024-01-13 00:20:17 +0530

    completed the image listing function

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -13,11 +13,14 @@ export default function CreateListing() {
     imageUrls: [],
   });
   const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
 
   console.log(formData);
 
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);// set the uploading state to true
+      setImageUploadError(false);// reset the error
       const promises = [];
 
       for (let i = 0; i < files.length; i++) {
@@ -30,13 +33,17 @@ export default function CreateListing() {
             imageUrls: formData.imageUrls.concat(urls), // add the new image urls to the form data
           }); // set the image urls in the form data
           setImageUploadError(false);
+          setUploading(false);
+          
         })
         .catch((error) => {
           console.log(error);
           setImageUploadError("image upload failed (2mb max per image)");
+          setUploading(false);
         });
     } else {
       setImageUploadError("image upload failed 6 image per listing");
+      setUploading(false);
     }
   };
 
@@ -62,6 +69,12 @@ export default function CreateListing() {
           });
         }
       );
+    });
+  };
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
   };
 
@@ -202,10 +215,11 @@ export default function CreateListing() {
             />
             <button
               type="button"
+              disabled={uploading}
               onClick={handleImageSubmit}
               className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
             >
-              Upload
+              {uploading ? "Uploading..." : "Upload"}
             </button>
           </div>
           <p className="text-red-700 text-sm">
@@ -215,18 +229,17 @@ export default function CreateListing() {
             formData.imageUrls.map((url, index) => (
               <div
                 key={url}
-                className="flex justify-between p-3 border items-center"
+                className='flex justify-between p-3 border items-center'
               >
                 <img
                   src={url}
-                  alt="listing image"
-                  className="w-20 h-20 object-contain rounded-lg"
+                  alt='listing image'
+                  className='w-20 h-20 object-contain rounded-lg'
                 />
                 <button
-                  type="button"
-                  disabled={uploading}
-                  onClick={handleImageSubmit}
-                  className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+                  type='button'
+                  onClick={() => handleRemoveImage(index)}
+                  className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'
                 >
                   Delete
                 </button>
